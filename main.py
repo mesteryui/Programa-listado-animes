2f872322c1e3662d5905a8d77a9b678bbcb1a49a
Mesteryui <oscarodriguez56@gmail.com>
2024-10-30 12:58:04 +0100

    Eliminando redundancia

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from busqueda_nombre import *
 from busqueda_genero import *
 from nuevo_anime import *
-from obtener_arbol import *
-# Cargamos el archivo XML
+
 def main():
     tipoBusqueda = input("Indique el tipo de busqueda a realizar:\n 1.Por nombre\n 2.Por genero\n 3.Añadir nuevo anime\n")
     if tipoBusqueda=="1":
